9883dbedbc03d3a49697f1914937afe44f376ed2
Hyon Lee <dooley994@gmail.com>
2018-01-03 01:32:14 -0500

    created a class structure for number to compartmentalize the code. added a do message in case the user enters no number to prevent crash

--- app/src/main/java/com/hyonlee/numbershape/MainActivity.java
+++ app/src/main/java/com/hyonlee/numbershape/MainActivity.java
@@ -9,64 +9,78 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    int inputNumber;
 
-    /*
-    Activated when the user presses the check button.
-    calls two functions that returns boolean value after checking if the entered value is a triangle or square value
-     */
-    public void checkValue(View view){
-        EditText number = (EditText) findViewById(R.id.input);
-        inputNumber = Integer.parseInt(number.getText().toString());
-        String message = "";
-        if(isTriangle()){
-            if(isSquare()){
-                message = "the number is both square and triangle";
+    class Number {
+        int num;
+        /**
+         * Checks if the input value is a triangle value
+         *
+         * @return If the entered value is triangle, returns boolean value of 'true', if not 'false'
+         */
+        public boolean isTriangle() {
+            int n = 1;
+            int TriangleNum = 1;
+            while (TriangleNum < num) {
+                n++;
+                TriangleNum = TriangleNum + n;
             }
-            message = "the number is triangle";
-        }else if(isSquare()){
-            message = "the number is square";
-        }else{
-            message = "the number is neither";
+            if (TriangleNum == num) {
+                return true;
+            } else {
+                return false;
+            }
         }
-        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+
+            /**
+             * Checks if the input value from the user is square
+             * @return if the value is square value, returns boolean value 'true' if not 'false'
+             */
+
+        public boolean isSquare() {
+            int y = 1;
+            int SquareNum = 1;
+            while (SquareNum < num) {
+                y++;
+                SquareNum = y * y;
+            }
+            if (SquareNum == num) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 
-    /**
-     * Checks if the input value is a triangle value
-     * @return If the entered value is triangle, returns boolean value of 'true', if not 'false'
-     */
-    public boolean isTriangle(){
-        int n = 1;
-        int TriangleNum =1;
-        while(TriangleNum < inputNumber){
-            n++;
-            TriangleNum = TriangleNum +n;
-        }
-        if(TriangleNum == inputNumber){
-            return true;
-        }else{
-            return false;
-        }
-    }
+        /*
+        Activated when the user presses the check button.
+        calls two functions that returns boolean value after checking if the entered value is a triangle or square value
+        */
+        public void checkValue(View view){
+            Number inputNumber = new Number();
+            EditText number = (EditText) findViewById(R.id.input);
+            if(number.getText().toString().isEmpty()){
+                Toast.makeText(getApplicationContext(), "Please enter a value", Toast.LENGTH_SHORT).show();
+            }else {
 
-    /**
-     * Checks if the input value from the user is square
-     * @return if the value is square value, returns boolean value 'true' if not 'false'
-     */
-    public boolean isSquare(){
-        int n = 1;
-        int SquareNum =1;
-        while(SquareNum < inputNumber){
-            n++;
-            SquareNum = n*n;
+
+                inputNumber.num = Integer.parseInt(number.getText().toString());
+
+                String message = "";
+                if (inputNumber.isTriangle()) {
+                    if (inputNumber.isSquare()) {
+                        message = "the number is both square and triangle";
+                    }
+                    message = "the number is triangle";
+                } else if (inputNumber.isSquare()) {
+                    message = "the number is square";
+                } else {
+                    message = "the number is neither";
+                }
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+            }
         }
-        if(SquareNum == inputNumber){
-            return true;
-        }else{
-            return false;
-        }
-    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
